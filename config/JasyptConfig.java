cfb36548094e46f42be51ef49ba7454eeb59b95e
woody35545 <woody35545@gmail.com>
2023-02-15 17:31:10 +0900

    Add JasyptConfig to encrypt sensitive imformation

---
+++ config/JasyptConfig.java
@@ -0,0 +1,20 @@
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
