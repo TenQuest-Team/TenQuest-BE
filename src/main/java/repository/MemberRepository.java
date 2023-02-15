34dd0aa67277f6b82cfbe9a752ecd54a5cd7df97
woody35545 <woody35545@gmail.com>
2023-02-15 18:28:33 +0900

    Change MemberRepository to interface

--- src/main/java/repository/MemberRepository.java
+++ src/main/java/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package repository;
 
 import entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
-public class MemberRepository extends  JpaRepository<UUID, Member>{
+@Repository
+public interface MemberRepository extends JpaRepository<UUID, Member> {
+
 }
