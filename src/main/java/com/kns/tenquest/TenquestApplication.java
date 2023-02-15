0eed3b78433ac4022e6a1f35eb8331c2fbeb1a2a
woody35545 <woody35545@gmail.com>
2023-02-15 17:09:15 +0900

    Initialize project files

---
+++ src/main/java/com/kns/tenquest/TenquestApplication.java
@@ -0,0 +1,13 @@
+package com.kns.tenquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kns/tenquest/TenquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kns.tenquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
