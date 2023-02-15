527facd86d07d4688ceaa3f46138c86b9107c0eb
woody35545 <woody35545@gmail.com>
2023-02-15 18:42:20 +0900

    Temporary Implement getAllMembers Method(For Testing)

--- src/main/java/service/MemberService.java
+++ src/main/java/service/MemberService.java
@@ -1,7 +1,20 @@
 package service;
 
+import entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.MemberRepository;
+
+import java.util.List;
+import java.util.UUID;
+
 
 @Service
 public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+    public List<Member> getAllMembers(){
+        // Temporarily implemented. Just for test.
+        return memberRepository.findAll();
+    }
 }
