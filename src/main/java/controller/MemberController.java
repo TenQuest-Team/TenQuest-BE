b5e94212531dfe52d6df75321ee8ee483093f440
woody35545 <woody35545@gmail.com>
2023-02-15 18:48:32 +0900

    Temporarily implement memberView method

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -1,7 +1,22 @@
 package controller;
 
+import entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
+    @Autowired
+    MemberService memberService;
+    @ResponseBody
+    @GetMapping("/view/members")
+    public List<Member> memberView(){
+        // Temporarily implemented. Just for test.
+        return memberService.getAllMembers();
+    }
 }
