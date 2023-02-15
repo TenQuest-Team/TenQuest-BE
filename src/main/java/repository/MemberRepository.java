c46f48a7e624cf21855d440ceac19f12f3b5c6a1
woody35545 <woody35545@gmail.com>
2023-02-15 18:42:30 +0900

    Fix typo

--- src/main/java/repository/MemberRepository.java
+++ src/main/java/repository/MemberRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface MemberRepository extends JpaRepository<UUID, Member> {
+public interface MemberRepository extends JpaRepository<Member, UUID> {
 
 }
