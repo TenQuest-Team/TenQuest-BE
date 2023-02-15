659988a2b845079899728e7abc93e009ae323e6d
woody35545 <woody35545@gmail.com>
2023-02-15 18:49:18 +0900

    Add lombook annotations

--- src/main/java/entity/Member.java
+++ src/main/java/entity/Member.java
@@ -1,9 +1,13 @@
 package entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 @Table(name = "member_table")
 @Entity
 public class Member{
