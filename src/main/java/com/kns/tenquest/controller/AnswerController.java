ccf407cdcb5cab72681e8178a18d6ce1b0195965
woody35545 <woody35545@gmail.com>
2023-02-17 14:05:06 +0900

    Implement answerView method for testing

--- src/main/java/com/kns/tenquest/controller/AnswerController.java
+++ src/main/java/com/kns/tenquest/controller/AnswerController.java
@@ -1,4 +1,25 @@
 package com.kns.tenquest.controller;
 
+import com.kns.tenquest.entity.Answer;
+import com.kns.tenquest.entity.Member;
+import com.kns.tenquest.service.AnswerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class AnswerController {
+    @Service
+    AnswerService answerService;
+
+    @GetMapping("/view/answers")
+    public String answerView(Model model){
+        // Temporarily implemented. Just for test.
+        List<Answer> answerList = answerService.getAllAnswers();
+        model.addAttribute("answerList", answerList);
+        return "answer_view";
+    }
 }
