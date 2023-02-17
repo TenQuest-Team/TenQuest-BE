6f944c2d710198bd3625c9e20b9143c52bff90ce
woody35545 <woody35545@gmail.com>
2023-02-17 13:32:18 +0900

    Create repository for replyer

---
+++ src/main/java/com/kns/tenquest/repository/ReplyerRepository.java
@@ -0,0 +1,7 @@
+package com.kns.tenquest.repository;
+
+import com.kns.tenquest.entity.Replyer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyerRepository extends JpaRepository<Replyer,Integer> {
+}
