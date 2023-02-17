f51d290e953cbed370561dc6f9e3dfc0cc82813c
woody35545 <woody35545@gmail.com>
2023-02-17 13:31:21 +0900

    Create service for replyer

---
+++ src/main/java/com/kns/tenquest/service/ReplyerService.java
@@ -0,0 +1,12 @@
+package com.kns.tenquest.service;
+import org.springframework.stereotype.Service;
+import com.kns.tenquest.entity.Replyer;
+import java.util.*;
+@Service
+public class ReplyerService {
+
+    public List<Replyer> getAllReplyers(){
+        return null;
+    }
+
+}
