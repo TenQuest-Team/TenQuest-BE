f5fe5355c5ab35e8071555cbb069e6bdcae86496
woody35545 <woody35545@gmail.com>
2023-02-17 13:38:34 +0900

    Temporarily implement replyerView method for testing

--- src/main/java/com/kns/tenquest/controller/ReplyerController.java
+++ src/main/java/com/kns/tenquest/controller/ReplyerController.java
@@ -1,4 +1,23 @@
 package com.kns.tenquest.controller;
 
+import com.kns.tenquest.entity.Replyer;
+import com.kns.tenquest.service.ReplyerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+@Controller
 public class ReplyerController {
+    @Autowired
+    ReplyerService replyerService;
+
+    @GetMapping("/view/replyers")
+    public String replyerView(Model model){
+        List<Replyer> replyerList = replyerService.getAllReplyers();
+        model.addAttribute("replyerList", replyerList);
+        return "replyer_view";
+
+    }
 }
