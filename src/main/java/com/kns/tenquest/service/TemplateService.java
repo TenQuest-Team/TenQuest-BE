ab0df0b72c77462568e62f802db068ecc8dd5e85
Son Jung Won <gkdlqkql6@naver.com>
2023-02-23 00:13:35 +0900

    Template CRUD 구현 Logic (Controller & Service)

--- src/main/java/com/kns/tenquest/service/TemplateService.java
+++ src/main/java/com/kns/tenquest/service/TemplateService.java
@@ -1,26 +1,30 @@
 package com.kns.tenquest.service;
 
-import com.kns.tenquest.entity.Member;
 import com.kns.tenquest.entity.Template;
-import com.kns.tenquest.repository.MemberRepository;
 import com.kns.tenquest.repository.TemplateRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TemplateService {
     @Autowired
-    private TemplateRepository templateRepository;
+    TemplateRepository templateRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
+    public List<Template> templateList(){
+        return templateRepository.findAll();
+    }
 
-    public void createTemplate(Template template){
-        Template newTemplate = new Template();
-        BeanUtils.copyProperties(template,newTemplate);
-        return
+    public void templatePost(Template template){
+        templateRepository.save(template);
+    }
 
+    public Template templateView(String id){
+        return templateRepository.findById(id).get();
+    }
 
+    public  void templateDelete(String id){
+        templateRepository.deleteById(id);
     }
 }
