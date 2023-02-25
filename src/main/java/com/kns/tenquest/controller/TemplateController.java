9225d2b0026e2bc2cdcedfb66bf76869b889eb74
garden <gkdlqkql6@naver.com>
2023-02-25 20:26:17 +0900

    Project Naming Convention 반영 / CRU 로직 변경 및 서비스 단으로 이동 / ResponseStatus return 하도록 수정

--- src/main/java/com/kns/tenquest/controller/TemplateController.java
+++ src/main/java/com/kns/tenquest/controller/TemplateController.java
@@ -1,13 +1,16 @@
 package com.kns.tenquest.controller;
 
-import com.kns.tenquest.entity.Template;
+import com.kns.tenquest.dto.ResponseDto;
+import com.kns.tenquest.dto.TemplateDto;
+import com.kns.tenquest.response.ResponseJson;
+import com.kns.tenquest.response.ResponseStatus;
 import com.kns.tenquest.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class TemplateController {
@@ -17,7 +20,7 @@ public class TemplateController {
 
     @GetMapping("/templates")
     public String templateView(Model model){
-        model.addAttribute("templateList",templateService.templateList());
+        model.addAttribute("templateList",templateService.getAllTemplates());
         return "template_list";
     } //template Read API
 
@@ -27,10 +30,16 @@ public class TemplateController {
     }
     //템플릿 생성 http GET 요청 (CREATE test용)
 
+    @ResponseBody
     @PostMapping("/templates")
-    public String templatePost(Template template){
-        templateService.templatePost(template);
-        return "redirect:/templates";
+    public ResponseJson<Integer> apiCreateTemplate(@RequestBody TemplateDto templateDto)throws NoSuchAlgorithmException {
+        int createResult = templateService.createTemplate(templateDto);
+
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
+        if(createResult == ResponseStatus.CREATE_FAIL.getCode()){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<Integer>(responseStatus,createResult).toResponseJson();
     } //template Create API
 
     @GetMapping("/templates/modify/{id}")
@@ -40,17 +49,19 @@ public class TemplateController {
         return "template_modify";
     } //변경 http GET 요청 (UPDATE Test용)
 
-    @PostMapping("/templates/update/{id}")
-    public String templateUpdate(@PathVariable("id") String id, Template template){
-        Template template1 = templateService.templateView(id);
-        template1.setTemplateName(template.getTemplateName());
-        templateService.templateSave(template1);
+    @PatchMapping("/templates/{id}")
+    public ResponseJson<Integer> apiTemplateUpdate(@PathVariable("id") String templateId, @RequestBody TemplateDto templateDto) throws NoSuchAlgorithmException {
+        int updateResult = templateService.templateUpdate(templateId, templateDto);
+        ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
 
-        return "redirect:/templates";
+        if(updateResult == ResponseStatus.CREATE_FAIL.getCode()){
+            responseStatus = ResponseStatus.CREATE_FAIL;
+        }
+        return new ResponseDto<Integer>(responseStatus,updateResult).toResponseJson();
     } //template Update API
 
     @GetMapping("/templates/view")
-    //PK를 소유한 하나의 레코드 정보로 들어가기
+    //HTML에서 PK를 받아 그 PK인 Id 소유한 하나의 레코드 정보로 들어가기
     public String templateView(Model model, String id){
         model.addAttribute("template",templateService.templateView(id));
         return "template_view";
