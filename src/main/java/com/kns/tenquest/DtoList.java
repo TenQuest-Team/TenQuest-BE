d6a5fe894ceb2887cf940c34f6a110ff2363fd22
woody35545 <woody35545@gmail.com>
2023-03-17 14:40:07 +0900

    [+] Implement DtoList

---
+++ src/main/java/com/kns/tenquest/DtoList.java
@@ -0,0 +1,52 @@
+package com.kns.tenquest;
+
+import com.kns.tenquest.dto.DataTransferObject;
+import com.kns.tenquest.response.Response;
+import com.kns.tenquest.response.ResponseStatus;
+import com.kns.tenquest.response.Responseable;
+import lombok.NoArgsConstructor;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+@NoArgsConstructor
+public class DtoList<E> extends ArrayList implements DataTransferObject, Responseable {
+    public <T> DtoList (List<T> anyList){
+        for (T elem : anyList){
+        this.add(elem);
+        }
+    }
+
+
+    @Override
+    public Object toEntity() throws NoSuchAlgorithmException {
+        return null;
+    }
+
+    @Override
+    public DataTransferObject toDto(Object entity) {
+        return null;
+    }
+
+    @Override
+    public Response toResponse(ResponseStatus responseStatus) {
+        return Responseable.super.toResponse(responseStatus);
+    }
+
+    @Override
+    public Object[] toArray(IntFunction generator) {
+        return super.toArray(generator);
+    }
+
+    @Override
+    public Stream stream() {
+        return super.stream();
+    }
+
+    @Override
+    public Stream parallelStream() {
+        return super.parallelStream();
+    }
+}
