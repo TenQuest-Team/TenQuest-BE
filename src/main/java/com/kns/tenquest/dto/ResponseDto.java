1bb0eee6ef07e6cc875434e55c951dcab7b125ca
woody35545 <woody35545@gmail.com>
2023-03-17 20:50:42 +0900

    [~] Solving the problem of returning like a two-dimensional array when converting to json format

--- src/main/java/com/kns/tenquest/dto/ResponseDto.java
+++ src/main/java/com/kns/tenquest/dto/ResponseDto.java
@@ -16,28 +16,34 @@ import java.util.List;
 public class ResponseDto<T> {
     private String status;
     private int code;
-    private List<T> responseDataList = new ArrayList<>();
+    private T data;
 
-    public ResponseDto(ResponseStatus responseStatus, List<T> responseDataList) {
-        this.status = responseStatus.getStatus();
-        this.code = responseStatus.getCode();
-        this.responseDataList = responseDataList;
-    }
+//    public ResponseDto(ResponseStatus responseStatus, List<T> responseDataList) {
+//        this.status = responseStatus.getStatus();
+//        this.code = responseStatus.getCode();
+//        this.data = responseDataList;
+//    }
     public ResponseDto(ResponseStatus responseStatus, T responseData) {
         this.status = responseStatus.getStatus();
         this.code = responseStatus.getCode();
         if (responseData instanceof T)
-        this.responseDataList.add(responseData);
+        this.data = responseData;
     }
 
-    public ResponseDto(String status, int code, List<T> responseDataList) {
+//    public ResponseDto(String status, int code, List<T> responseDataList) {
+//        this.status = status;
+//        this.code = code;
+//        this.data = responseDataList;
+//    }
+
+    public ResponseDto(String status, int code, T responseData) {
         this.status = status;
         this.code = code;
-        this.responseDataList = responseDataList;
+        this.data = responseData;
     }
 
     public Response<T> toResponse(){
-    return new Response<>((T) new ResponseDto<T>(this.getStatus(),this.getCode(), this.responseDataList),
+    return new Response<>((T) new ResponseDto<T>(this.getStatus(),this.getCode(), data),
             new HttpHeaders(),
             this.getCode());
     }
