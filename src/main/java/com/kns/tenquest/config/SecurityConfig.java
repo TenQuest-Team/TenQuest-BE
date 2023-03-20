2952b4cd4e630f8f02d549765c5ff4f769a2e659
woody35545 <woody35545@gmail.com>
2023-03-21 04:03:51 +0900

    [+] Add Spring-Security dependency and initialize configuration

---
+++ src/main/java/com/kns/tenquest/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.kns.tenquest.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/members/**").authenticated()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+}
