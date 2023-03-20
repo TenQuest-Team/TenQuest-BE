29d2f6e03ad1569ad6ae7abac4106d44bfc61e33
woody35545 <woody35545@gmail.com>
2023-03-21 04:49:37 +0900

    [>] Filter Testing

--- src/main/java/com/kns/tenquest/config/FilterConfig.java
+++ src/main/java/com/kns/tenquest/config/FilterConfig.java
@@ -1,7 +1,6 @@
 package com.kns.tenquest.config;
 
 import com.kns.tenquest.filter.secFilter1;
-import jakarta.servlet.FilterRegistration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,7 @@ public class FilterConfig {
     @Bean
     public FilterRegistrationBean<secFilter1> secFilter1(){
         FilterRegistrationBean<secFilter1> filterRegistrationBean = new FilterRegistrationBean<secFilter1>(new secFilter1());
-        filterRegistrationBean.addUrlPatterns("/**");
+        filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setOrder(0);
         return filterRegistrationBean;
     }
--- src/main/java/com/kns/tenquest/filter/secFilter1.java
+++ src/main/java/com/kns/tenquest/filter/secFilter1.java
@@ -1,5 +1,7 @@
 package com.kns.tenquest.filter;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
@@ -11,7 +13,26 @@ public class secFilter1 implements Filter{
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Sec Filter 1");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+
+        // System.out.println("sec filter 1");
+
+        /* get "Authorization" field value from header */
+        String authField = req.getHeader("Authorization");
+        System.out.println("Auth: " + authField);
+
+        if(authField.equals("sec"))
+        {
+            res.getWriter().println("Auth Failed"); // for check
+            chain.doFilter(req,res);
+        }
+        else res.getWriter().println("Auth Failed"); // for check
+
     }
 
     @Override
