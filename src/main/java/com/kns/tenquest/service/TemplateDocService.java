f86fc56c4e3fad23ad5368c6628f1b69cb2afa88
garden <gkdlqkql6@naver.com>
2023-03-23 21:40:18 +0900

    서비스 어노테이션 맵핑 / 오토와이어 작성

--- src/main/java/com/kns/tenquest/service/TemplateDocService.java
+++ src/main/java/com/kns/tenquest/service/TemplateDocService.java
@@ -1,4 +1,11 @@
 package com.kns.tenquest.service;
 
+import com.kns.tenquest.repository.TemplateDocRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TemplateDocService {
+    @Autowired
+    TemplateDocRepository templateDocRepository;
 }
