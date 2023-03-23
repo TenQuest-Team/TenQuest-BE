b3dec6c34e76448a535e995a2d42ba364fa19894
woody35545 <woody35545@gmail.com>
2023-03-23 23:07:56 +0900

    [+] Add Json-simple dependency

--- src/main/java/com/kns/tenquest/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kns/tenquest/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.kns.tenquest.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kns.tenquest.entity.Member;
 import io.micrometer.observation.annotation.Observed;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +10,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.SortedMap;
+
 // UsernamePasswordAuthenticationFilter runs when /login(post) request
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -15,7 +22,23 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        System.out.println("Attempt Login!!");
+        System.out.println("Login Request Occur!");
+        /* get user's id, password from login request */
+//        try {
+//            BufferedReader reqReader = request.getReader();
+//            String input = null;
+//            while((input= reqReader.readLine()) != null)
+//                System.out.println(input);
+//            }
+//        catch (IOException e) {e.printStackTrace();}
+
+        /* mapping Json type user id,pw information to Member Entity */
+        try {
+            //JSONParser jsonParser = new JSONParser();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return super.attemptAuthentication(request, response);
     }
 
