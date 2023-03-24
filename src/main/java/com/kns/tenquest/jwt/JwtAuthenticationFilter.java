b6cc25ad26ac694f76423618e980d64ace85e57c
woody35545 <woody35545@gmail.com>
2023-03-24 10:11:45 +0900

    [~] JWT filter testing

--- src/main/java/com/kns/tenquest/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kns/tenquest/jwt/JwtAuthenticationFilter.java
@@ -3,16 +3,21 @@ package com.kns.tenquest.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kns.tenquest.entity.Member;
 import io.micrometer.observation.annotation.Observed;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StreamUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.SortedMap;
 
 // UsernamePasswordAuthenticationFilter runs when /login(post) request
@@ -34,7 +39,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         /* mapping Json type user id,pw information to Member Entity */
         try {
-            //JSONParser jsonParser = new JSONParser();
+            ServletInputStream inputStream = request.getInputStream();
+            String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+//            BufferedReader reqReader = request.getReader();
+//            String input = null;
+//            while((input= reqReader.readLine()) != null)
+//                System.out.println(input);
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObj = (JSONObject) jsonParser.parse(messageBody);
+            String requestBodyUserName = jsonObj.get("username").toString();
+            String requestBodyUserPassword = jsonObj.get("password").toString();
+
+            System.out.println("RequestBody[\"username\"]: " + requestBodyUserName); //test complete
+            System.out.println("RequestBody[\"password\"]: " + requestBodyUserPassword); //test complete
+
+            Member member = Member.builder()
+                    .userId(requestBodyUserName)
+                    .userInfo(requestBodyUserPassword)
+                    .build();
+
+            System.out.println("member.userId: " + member.getUserId()); //test complete
+            System.out.println("member.password: " + member.getUserInfo()); //test complete
+
 
         }catch(Exception e){
             e.printStackTrace();
