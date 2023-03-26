7bf57d3f4da9c76e6b6369d53db8ca59f9b66182
garden <gkdlqkql6@naver.com>
2023-03-27 01:04:24 +0900

    createTemplate 로직에 templateDoc도 생성하도록 수정 / @Transactional 사용해서 로직 중 에러 발생 시 두 테이블 다 생성하지 못하도록 설정

--- src/main/java/com/kns/tenquest/service/TemplateService.java
+++ src/main/java/com/kns/tenquest/service/TemplateService.java
@@ -3,17 +3,23 @@ package com.kns.tenquest.service;
 import com.kns.tenquest.DtoList;
 import com.kns.tenquest.RequestWrapper.CreateTemplateRequestWrapper;
 import com.kns.tenquest.dto.ResponseDto;
+import com.kns.tenquest.dto.TemplateDocDto;
 import com.kns.tenquest.dto.TemplateDto;
 import com.kns.tenquest.entity.Member;
 import com.kns.tenquest.entity.Template;
+import com.kns.tenquest.entity.TemplateDoc;
 import com.kns.tenquest.repository.MemberRepository;
+import com.kns.tenquest.repository.TemplateDocRepository;
 import com.kns.tenquest.repository.TemplateRepository;
+import com.kns.tenquest.requestBody.TemplateRequestBody;
 import com.kns.tenquest.response.ResponseStatus;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,6 +28,9 @@ import java.util.UUID;
 public class TemplateService {
     @Autowired
     TemplateRepository templateRepository;
+
+    @Autowired
+    TemplateDocRepository templateDocRepository;
 
     @Autowired
     MemberRepository memberRepository;
@@ -37,20 +46,33 @@ public class TemplateService {
     public TemplateDto getTemplateByTemplateName(String templateName){
         return new TemplateDto(templateRepository.findTemplateByTemplateName(templateName).orElse(new Template()));
     }
+
+    @Transactional
     public CreateTemplateRequestWrapper createTemplate(CreateTemplateRequestWrapper requestWrapper, String memberId) {
         Optional<Member> nullableMember = memberRepository.findById(memberId);
         if(nullableMember.isEmpty()){
             throw new NoSuchElementException("존재하지 않는 사용자 입니다.");
         }
-        Optional<Template> optTemplate = templateRepository.findTemplateByTemplateNameAndTemplateOwner(requestWrapper.getTemplateDto().templateName,memberId);
+        TemplateDto creatingTemplate = requestWrapper.getTemplateDto();
+        Optional<Template> optTemplate = templateRepository.findTemplateByTemplateNameAndTemplateOwner(creatingTemplate.templateName,memberId);
         if (optTemplate.isEmpty()) {
             try {
                 String thisTemplateId = UUID.randomUUID().toString().replace("-", "");
-                requestWrapper.getTemplateDto().setCreatedAt(LocalDateTime.now());
-                requestWrapper.getTemplateDto().setTemplateId(thisTemplateId);
-                requestWrapper.getTemplateDto().setTemplateOwner(memberId);
-                requestWrapper.getTemplateDto().setIsPublic(true);
-                templateRepository.save(requestWrapper.getTemplateDto().toEntity());
+                creatingTemplate.setCreatedAt(LocalDateTime.now());
+                creatingTemplate.setTemplateId(thisTemplateId);
+                creatingTemplate.setTemplateOwner(memberId);
+                creatingTemplate.setIsPublic(true);
+                templateRepository.save(creatingTemplate.toEntity());
+                //template 생성 로직
+
+                List<TemplateDocDto> creatingTemplateDocList = requestWrapper.getTemplateDocList();
+                for(int i=0;i<creatingTemplateDocList.size();i++){
+                    TemplateDocDto creatingTemplateDoc = creatingTemplateDocList.get(i);
+                    creatingTemplateDoc.setTemplateId(thisTemplateId);
+                    templateDocRepository.save(creatingTemplateDoc.toEntity());
+                }
+                //templateDoc 생성 로직
+
                 return requestWrapper;
             } catch (RuntimeException e) {
                 e.printStackTrace();
@@ -92,5 +114,34 @@ public class TemplateService {
         templateRepository.deleteById(templateId);
         return deletedTemplateDto;
 
+    }
+
+    //건모형이 만든거 (참고용)
+    public TemplateDto _addTemplate(TemplateRequestBody templateRequestBody){
+        /* Just Example. Implement Needed */
+
+        // generate template id
+        String generatedTemplateId = UUID.randomUUID().toString().replace("-", "");
+
+        // 1. add template doc to db
+        for (int i=0; i<templateRequestBody.QuestionDocuments.size(); i++){
+            TemplateDoc templateDoc = TemplateDoc.builder()
+                    .templateDocId((long)i)
+                    .templateId(generatedTemplateId)
+                    .questionId(templateRequestBody.QuestionDocuments.get(i))
+                    .questionOrder(templateRequestBody.QuestionOrder.get(i))
+                    .build();
+                    templateDocRepository.save(templateDoc);
+        }
+
+        // 2. add template to db
+        Template template = Template.builder()
+                .templateId(generatedTemplateId)
+                .templateOwner(templateRequestBody.templateOwner)
+                .templateName(templateRequestBody.templateName)
+                .build();
+
+        templateRepository.save(template);
+        return new TemplateDto();
     }
 }
