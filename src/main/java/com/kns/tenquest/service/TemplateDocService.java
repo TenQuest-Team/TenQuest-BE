514e7617198c4603361b128a6f7e077724344072
garden <gkdlqkql6@naver.com>
2023-03-26 21:08:17 +0900

    getAllTemplateDocs 생성 / createDocs 생성중

--- src/main/java/com/kns/tenquest/service/TemplateDocService.java
+++ src/main/java/com/kns/tenquest/service/TemplateDocService.java
@@ -1,5 +1,9 @@
 package com.kns.tenquest.service;
 
+import com.kns.tenquest.DtoList;
+import com.kns.tenquest.dto.ResponseDto;
+import com.kns.tenquest.dto.TemplateDocDto;
+import com.kns.tenquest.dto.TemplateDto;
 import com.kns.tenquest.repository.TemplateDocRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,13 @@ import org.springframework.stereotype.Service;
 public class TemplateDocService {
     @Autowired
     TemplateDocRepository templateDocRepository;
+
+    public DtoList<TemplateDocDto> getAllTemplateDocs(){
+        DtoList<TemplateDocDto> templateDocDtoList = new DtoList<>(templateDocRepository.findAll());
+        return templateDocDtoList;
+    }
+
+//    public ResponseDto<TemplateDocDto> createTemplateDoc (DtoList<TemplateDocDto> )
+
+
 }
