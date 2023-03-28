7181fcf296c755fc2b2ac63136cc483358244411
woody35545 <woody35545@gmail.com>
2023-03-28 23:00:54 +0900

    Implement API related to replyerNameList for temlate

--- src/main/java/com/kns/tenquest/controller/AnswerController.java
+++ src/main/java/com/kns/tenquest/controller/AnswerController.java
@@ -2,14 +2,16 @@ package com.kns.tenquest.controller;
 import com.kns.tenquest.DtoList;
 import com.kns.tenquest.dto.AnswerDto;
 
-import com.kns.tenquest.requestBody.AnswerCreateRequestBody;
+import com.kns.tenquest.dto.ResponseDto;
+import com.kns.tenquest.requestBody.SingleAnswerCreateRequestBody;
 import com.kns.tenquest.response.Response;
 import com.kns.tenquest.response.ResponseStatus;
 import com.kns.tenquest.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import com.kns.tenquest.ENV;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping(ENV.API_PREFIX)
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -23,7 +25,7 @@ public class AnswerController {
     }
 
     @GetMapping("/answers/docId")
-    public Response<AnswerDto> apiGetAnswerByDocId(@RequestParam("value") String docId){
+    public Response<AnswerDto> apiGetAnswerByDocId(@RequestParam("value") Long docId){
         DtoList<AnswerDto> answerDtoList = answerService.getAnswerByDocId(docId);
         ResponseStatus responseStatus = ResponseStatus.OK;
 
@@ -34,9 +36,9 @@ public class AnswerController {
     }
 
     @PostMapping("/answers")
-    public Response<AnswerDto> apiCreateAnswer(@RequestBody AnswerCreateRequestBody answerCreateRequestBody){
+    public Response<AnswerDto> apiCreateAnswer(@RequestBody SingleAnswerCreateRequestBody singleAnswerCreateRequestBody){
         ResponseStatus responseStatus = ResponseStatus.CREATE_DONE;
-        AnswerDto answerDto = answerService.createAnswer(answerCreateRequestBody);
+        AnswerDto answerDto = answerService.createSingleAnswer(singleAnswerCreateRequestBody);
 
         if(answerDto.answerId == null){
             responseStatus = ResponseStatus.CREATE_FAIL;
@@ -44,5 +46,23 @@ public class AnswerController {
         return answerDto.toResponse(responseStatus);
     }
 
+    @GetMapping("/answers/replyerNames")
+    public Response<List> apiGetAnswersReplyerNameListByTemplateId(@RequestParam("value") String templateId){
+
+        return new ResponseDto<List>
+                (ResponseStatus.OK,
+                        answerService
+                                .getReplyerNameListByTemplateId(templateId))
+                .toResponse();
+    }
+    @GetMapping("/answers/replyerId")
+    public Response<List> apiGetAnswersByReplyerId(@RequestParam("value") int replyerId){
+
+        return new ResponseDto<List>
+                (ResponseStatus.OK,
+                        answerService
+                                .getAnswerListByReplyerId(replyerId))
+                .toResponse();
+    }
 
 }
