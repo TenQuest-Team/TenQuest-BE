fcb8ed3c26594e45eae0b7dceb4e7a78108b83aa
Shsin9797 <aaa8141@naver.com>
2023-04-25 11:52:38 +0900

    categoryEnum 수정 -> enum Response 같은 형태로

--- src/main/java/com/kns/tenquest/CategoryEnum.java
+++ src/main/java/com/kns/tenquest/CategoryEnum.java
@@ -1,22 +1,28 @@
 package com.kns.tenquest;
 
 public enum CategoryEnum {
-    USERCREATED(0),
-    PERSONALITY(1),
-    APPEARANCE(2),
-    RELATIONSHIP(3);
+    USER_CREATED(0,"USER_CREATED"),
+    PERSONALITY(1,"PERSONALITY"),
+    APPEARANCE(2,"APPEARANCE"),
+    RELATIONSHIP(3,"RELATIONSHIP");
 
     private final int id;
+    private final String categoryName;
 
-    CategoryEnum(int id) {
+    CategoryEnum(int id,String categoryName) {
         this.id = id;
+        this.categoryName=categoryName;
     }
+
 
     public int getId() {
         return id;
     }
 
-    public static CategoryEnum fromId(int id) {   // CategoryEnum.fromId(1) 하면 CategoryEnum.PERSONALITY 반환함
+
+
+    // CategoryEnum.fromId(1) 하면 CategoryEnum.PERSONALITY 반환하도록 하는 코드
+    public static CategoryEnum fromId(int id) {
         for (CategoryEnum categoryEnum : CategoryEnum.values()) {
             if (categoryEnum.id == id) {
                 return categoryEnum;
