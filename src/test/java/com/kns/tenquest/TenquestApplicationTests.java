4e4c7d8dd1d12896887e41d333d02938720f2658
woody35545 <woody35545@gmail.com>
2023-09-23 03:55:18 +0900

    [+] Logback 설정 추가

--- src/test/java/com/kns/tenquest/TenquestApplicationTests.java
+++ src/test/java/com/kns/tenquest/TenquestApplicationTests.java
@@ -1,7 +1,12 @@
 package com.kns.tenquest;
 
 import com.kns.tenquest.entity.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.kns.tenquest.repository.MemberRepository;
